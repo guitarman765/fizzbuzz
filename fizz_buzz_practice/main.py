a9f6f101bf74930931fcc287acbad6a0678e3427
guitarman765 <74016487+guitarman765@users.noreply.github.com>
2020-11-18 11:45:24 -0500

    Updating program requirements
    Updating the fizz buzz program description to make the program more user friendly and dynamic.

--- fizz_buzz_practice/main.py
+++ fizz_buzz_practice/main.py
@@ -1,7 +1,8 @@
-# Create fizz buzz program that takes numbers 1 - 100 and prints fizz if the number is evenly divisible by 2
-# Create fizz buzz program that takes numbers 1 - 100 and prints buzz if the number is evenly divisible by 3
-# Create fizz buzz program that takes numbers 1 - 100 and prints fizzbuzz if the number is evenly divisible by 2 and 3
-
+# Create A program that takes in a user defined number, two divisible by numbers and two keywords.
+# The program checks every number up to the 1st user defined number.
+# If the number is evenly divisible by the users 2nd input number the program prints a key word.
+# If the number is evenly divisible by the users 3rd input number the program prints a second key word.
+# If the number is evenly divisible by the both the users 2nd and 3rd input numbers a concatenated keyword is printed.
 
 i = 1
 while i < 100 + 1:
